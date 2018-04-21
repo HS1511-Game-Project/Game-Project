611590bc74a733210fe9a30d08c2f06b4ecd6b41
Djimon Jayasundera <woofless77@gmail.com>
2018-04-21 20:39:44 +0800

    Update testGame.c

--- testGame.c
+++ testGame.c
@@ -7,11 +7,18 @@
 
 #define BOARD_SIZE 100
 #define CAMPUS 1
+#define BACHELOR_OF_MONEY 1
+
 int main(/* Add argv stuff here */) {
   // TEST 1 // buildCampus, getTileInfo
   char * newBoard[BOARD_SIZE] = setBoardUp();
   newBoard = buildCampus("lr");
   assert(getTileInfo("lr", newBoard) == CAMPUS);
-  // TEST 2 // 
+  // TEST 2 // getPlayerInfo, addStudent
+  int playerToTest = 1;
+  int initialNumberOfStudents = getPlayerInfo(playerToTest).bMS; // bMS stands for Bachelor of Money Students
+  addStudent(BACHELOR_OF_MONEY, playerToTest, getAllPlayersInfo());
+  int newNumberOfStudents = getPlayerInfo(playerToTest).bMS;
+  assert(newNumberOfStudents == initialNumberOfStudents + 1);
   
 }
