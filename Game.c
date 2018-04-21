17d33008ffabe30fdc42de9c35fd14cff9297419
Djimon Jayasundera <woofless77@gmail.com>
2018-04-21 14:24:59 +0800

    Update Game.c

--- Game.c
+++ Game.c
@@ -1,1 +1,9 @@
+// Created by [insert your names here]
+// This programs aims to host a game for AIs to play on.
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(/* Put the argv argc stuff here plz */) {
+  
+}
