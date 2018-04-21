cdeda620ec6cc21e34c64f6ec1f8fb62b1cfca66
Djimon Jayasundera <woofless77@gmail.com>
2018-04-21 14:26:49 +0800

    Update testGame.c

--- testGame.c
+++ testGame.c
@@ -1,1 +1,10 @@
+// Created by [insert names here]
+// This program aims to test the game and see if it is valid.
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "Game.h"
+
+int main(/* Add argv stuff here */) {
+  
+}
