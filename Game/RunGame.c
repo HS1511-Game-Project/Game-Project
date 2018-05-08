ca70352cccc377eb169000c7a33f3627ef1a0656
Djimon Jayasundera <woofless77@gmail.com>
2018-05-08 09:31:25 +0800

    Update RunGame.c

--- Game/RunGame.c
+++ Game/RunGame.c
@@ -2,3 +2,29 @@
 // To host the game
 
 #include "Game.h"
+
+
+Game g = newGame (disciplines, dice); // Create game
+bool gameOver = False;
+
+while (!gameOver) {      
+    diceValue = rollDice();    //simulate throw of two dice - we will discuss how to do this in tutorial
+                               // or you may wish to google it and find out how you could do it! 
+    dicevalue += rollDice();    //roll second dice 
+    
+    throwDice(g,diceValue);      
+    
+    action move; //human or mechanicalTurk(AI) decide what they want to do     
+
+    //loop until player action is PASS or player wins     
+    while (turn is not over and game not over) { 
+        move = decideAction(g); 
+        assert that action is legal 
+        if (move.actionCode == START_SPINOFF) { 
+             //decide if outcome is patent or publication                     
+        }         
+        makeAction(g, move);             
+     } 
+} 
+//print statistics 
+// free memory
