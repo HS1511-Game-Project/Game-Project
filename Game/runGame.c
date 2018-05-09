3daeb7999275a0416b607bf8a708b7efa9031853
Djimon Jayasundera <woofless77@gmail.com>
2018-05-10 07:39:06 +0800

    Add files via upload

--- Game/runGame.c
+++ Game/runGame.c
@@ -1,30 +1,43 @@
-// Created by Harris Borman, Djimon Jayasundera, and Zachary Chengy
-// To host the game
+// By Djimon Jayasundera, Zac Cheng, and Harris Borman
+// 5/9/2018
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
 #include "Game.h"
+#include "mechanicalTurk.h"
+
+#define DEFAULT_DISCIPLINES {STUDENT_BQN, STUDENT_MMONEY, STUDENT_MJ, \
+  STUDENT_MMONEY, STUDENT_MJ, STUDENT_BPS, STUDENT_MTV, \
+  STUDENT_MTV, STUDENT_BPS, STUDENT_MTV, STUDENT_BQN, \
+  STUDENT_MJ, STUDENT_BQN, STUDENT_THD, STUDENT_MJ, \
+  STUDENT_MMONEY, STUDENT_MTV, STUDENT_BQN, STUDENT_BPS}
+#define DEFAULT_DICE {9,10,8,12,6,5,3,11,3,11,4,6,4,7,9,2,8,10,5}
 
 
-Game g = newGame (disciplines, dice); // Create game
-bool gameOver = False;
-
-while (game not over) {      
-    diceValue = rollDice();    //simulate throw of two dice - we will discuss how to do this in tutorial
-                               // or you may wish to google it and find out how you could do it! 
-    dicevalue += rollDice();    //roll second dice 
-    
-    throwDice(g,diceValue);      
-    
-    action move; //human or mechanicalTurk(AI) decide what they want to do     
-
-    //loop until player action is PASS or player wins     
-    while (turn is not over and game not over) { 
-        move = decideAction(g); 
-        assert that action is legal 
-        if (move.actionCode == START_SPINOFF) { 
-             //decide if outcome is patent or publication                     
-        }         
-        makeAction(g, move);             
-     } 
-} 
-//print statistics 
-// free memory
+int main() {
+  int disciplines[] = DEFAULT_DISCIPLINES;
+  int dice[] = DEFAULT_DICE;
+  Game g = newGame(disciplines, dice);
+  int gameOver = getKPIpoints(g, UNI_A)>=150 && getKPIpoints(g, UNI_B)>=150 && getKPIpoints(g, UNI_C)>=150;
+  while (!gameOver) {
+    int diceValue = throwDice(g, dice);
+    action move;
+    int currentTurn = getWhoseTurn(g);
+    gameOver = getKPIpoints(g, UNI_A)<150 && getKPIpoints(g, UNI_B)<150 && getKPIpoints(g, UNI_C)<150;
+    while (getWhoseTurn(g) == currentTurn && !gameOver) {
+      move = decideAction(g);
+      assert(isLegalAction(g, move) == TRUE);
+      if (move.actionCode == START_SPINOFF) {
+        int decision = rand()%6+1; // Number between 1 and 6 (inclusive)
+        if (decision == 1 || decision == 2) {
+          move.actionCode = OBTAIN_PUBLICATION;
+        } else {
+          move.actionCode = OBTAIN_IP;
+        }
+      }
+      makeAction(g, move);
+    }
+  }
+  return EXIT_SUCCESS;
+}
